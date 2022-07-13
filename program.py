aeb00614841bc922b00b77a7312e809f3414db83
Sam Crabtree <samuel.crabtree@protonmail.com>
2022-07-13 18:46:28 -0400

    Major Overhaul
    "Some Edits"
    *Sarcasm*

--- program.py
+++ program.py
@@ -2,103 +2,110 @@ import pandas as pd
 import requests
 import json
 import os
-from pandas import json_normalize
-import ssl
 
 
 
-ssl._create_default_https_context = ssl._create_unverified_context
+
 
 # IMPORT DATA FROM COLLEGE SCORECARD ONLINE
 api_url = "https://api.data.gov/ed/collegescorecard/v1/"
 api_key = ""
-dataset = "schools.json?"
-fields = [
+
+school_dataset = "schools"
+school_fields = [
+
     #SCHOOL INFORMATION
     "school.name",
     "school.city",
     "school.state",
     "school.degrees_awarded.predominant_recoded",
-    # Data Definitions
-    #  0 - Not classified
-    #  1 - Predominantly certificate-degree granting (varies)
-    #  2 - Predominantly associate's-degree granting (2)
-    #  3 - Predominantly bachelor's-degree granting (4)
-    #  4 - Entirely graduate-degree granting (5+)
-    "school.ownership",
-    # Data Definitions
-    #  1 - Public
-    #  2 - Private Non-Profit
-    #  3 - Private For Profit
+
+    # Data Definitions 0 - Not classified, 1 - Predominantly certificate-degree granting (varies),
+    # 2 - Predominantly associate's-degree granting (2), 3 - Predominantly bachelor's-degree granting (4)
+    # 4 - Entirely graduate-degree granting (5+)
+
+    "school.ownership", # Data Definitions #  1 - Public, 2 - Private Non-Profit, 3 - Private For Profit
 
     #SCHOOL BODY DETAILS
-    "school.men_only",
-    "school.women_only",
-    "school.minority_serving.historically_black",
-    "school.minority_serving.predominantly_black",
-    "school.minority_serving.annh", # Alaska Native / Native Hawaiian Serving Institution
-    "school.minority_serving.nant", # Native American Non-Tribal Serving Institution
-    "school.minority_serving.tribal", # Native Tribal Serving Institution
-    "school.minority_serving.aanipi", # Native American Pacific Islander Serving Institution
-    "school.minority_serving.hispanic", # Hispanic Serving Institution
     "school.religious_affiliation",
-
-    #ADMISSIONS NUMBERS
-    "admissions.admission_rate_overall",
-
+    "2020.admissions.test_requirements",
+    "2020.admissions.sat_scores.average.overall",
+    "2020.admissions.admission_rate.overall",
+    "2020.cost.tuition.in_state",
+    "2020.cost.tuition.out_of_state",
+    "2020.cost.tuition.program_year",
 ]
 
-#Builds out the API Query URL
-response_api = requests.get( api_url + dataset + \
-              "&fields=id" + "&per_page=100" + "&api_key=" + api_key)
+# Builds out the API Query URL
 
-data = response_api.text
-parse_json = json.loads(data)
+def api_query():
+    response_api = requests.get( api_url + school_dataset + ".json?" + \
+                                 "&fields=id" + "&per_page=100" + "&api_key=" + api_key)
+    data = response_api.text
+    parse_json = json.loads(data)
+    total = parse_json['metadata']['total']
+    pages = round(total / 100) #Takes the total number of colleges and divides it by the results per page setting to
+                               # generate the number of API pages.
+
+    return pages
+
+# Variable storage for the above function.
+num_pages = api_query()
 
 
-#Finds the total amount of colleges in the list.
-total = parse_json['metadata']['total']
+#API SEARCH FUNCTION
 
-#Takes the total number of colleges and divides
-# it by the results per page setting to generate
-# the number of API pages.
-num_pages = round(total / 100)
+# Builds out the API Query URL
+def api_pull(dataset, fields):
+    data_pull = []
+    i = 0
+    for page in range(num_pages):
+        api_pull_url = requests.get(api_url + dataset + ".json?" +  \
+              "&page=" + str(i) + "&fields=" + ",".join(fields) + "&per_page=100" + "&api_key=" + api_key )
+        print("Downloading College Scorecard API Results for " + dataset.title() + " Page " +str(i) +
+              " of " + str(num_pages) + "...")
+        i += 1
 
-#Indicates the number of school entries
-schools = []
+        response = api_pull_url.text
+        response_json = json.loads(response)
 
-#Iteration variable
-i = 0
+        data_pull.extend(response_json['results'])
 
-#This loop allows for the program to pull all of the from the API with a number of requests
-for page in range(num_pages):
-    url = api_url + dataset + \
-          "&page=" + str(i) + "&fields=" + ",".join(fields) + "&per_page=100" + "&api_key=" + api_key
-    response = requests.get(url)
-    data = response.json()
-    print("Downloading College Scorecard API Results Page " + str(i) + " of " + str(num_pages) + "...")
-    i += 1
+    return data_pull
 
-    schools.extend(data['results'])
 
+
+print("Beginning Program...")
+api_query()
+schools = api_pull(school_dataset, school_fields)
+
+#TAKES ALL API RESULTS AND CREATES PANDAS DATAFRAME
 df1 = pd.DataFrame(schools)
 
+# REORDER RESULTS COLUMNS
+df1 = df1[["school.name", "school.city", "school.state", "school.degrees_awarded.predominant_recoded",
+               "school.ownership", "school.religious_affiliation", "2020.admissions.test_requirements",
+               "2020.admissions.sat_scores.average.overall", "2020.admissions.admission_rate.overall",
+               "2020.cost.tuition.in_state", "2020.cost.tuition.out_of_state", "2020.cost.tuition.program_year", ]]
 
+#RENAME API COLUMNS
+df1.columns = ["Institution Name", "City", "State", "Primary Degree Offered", "Ownership", "Religious Affiliation",
+               "School Test Requirements",
+               "Test Averages", "Total Admissions Rate","In State Tuition Academic Year",
+               "Out of State Tuition Academic Year", "Program Year Cost",
+               ]
 
+# Import Data From Multiple CSV Files Found at http://www.tuitiontracker.org/ NOTE: STILL WAITING FOR API ACCESS
+# FOR THIS DATA SOURCE THIS CSV FILE CAN BE FOUND ON TUITION TRACKER.COM
 
+df2 = pd.read_csv('http://www.tuitiontracker.org/data/download/cost-attendance.csv',
+                  usecols=['Institution Name', 'Published in-state tuition and fees 2017-18 (IC2017_AY)',
+                             ])
 
-# #Import Data From Multiple CSV Files Found at https://www.tuitiontracker.org/
-# colleges_df = pd.read_csv('https://www.tuitiontracker.org/data/download/institutions.csv', usecols = ['UnitID', 'Institution Name'])
-df2 = pd.read_csv('https://www.tuitiontracker.org/data/download/cost-attendance.csv', usecols = ['Institution Name',
-                                                                                                 'Published in-state tuition and fees 2017-18 (IC2017_AY)',
+# Renames all of the Dataframes values above
+df2.columns = ['Institution Name', 'In State Tuition 17-18',]
 
-
-
-                                                                                                 ])
-# #Imports Data Regarding
-# net_tuition_df = pd.read_csv('https://www.tuitiontracker.org/data/download/net-price.csv', usecols = ['UnitID', 'Institution Name'])
-#
-#Imports Data Regarding Graduation Rates NOTE: STILL WAITING FOR API ACCESS FOR THIS DATA SOURCE THIS CSV FILE CAN BE FOUND ON TUITION TRACKER.COM
+#Imports Data Regarding Graduation Rates
 df3 = pd.read_csv('http://www.tuitiontracker.org/data/download/grad-rates.csv',
                            usecols = ["Institution Name",
                                       "Grand total (GR2016  Bachelor's or equiv subcohort (4-yr institution) CALCULATED GRADUATION RATE)",
@@ -115,20 +122,18 @@ df3 = pd.read_csv('http://www.tuitiontracker.org/data/download/grad-rates.csv',
                                       "Grand total (GR2011  Degree/certif-seeking students ( 2-yr institution) CALCULATED GRADUATION RATE)",
                                       ])
 
-#This Merges the above Dataframes by the institution name (which is listed as school.name on Gov College Score Card
-#and "Institution Name" at Tuition Tracker
+# Renames all of the Dataframes values above
+df3.columns = ["Institution Name", "2016 4 YR Grad Rate", "2015 4 YR Grad Rate", "2014 4 YR Grad Rate",
+               "2013 4 YR Grad Rate", "2012 4 YR Grad Rate", "2011 4 YR Grad Rate", "2016 2 YR Grad Rate",
+               "2015 2 YR Grad Rate", "2014 2 YR Grad Rate", "2013 2 YR Grad Rate", "2012 2 YR Grad Rate",
+               "2011 2 YR Grad Rate",
+               ]
 
+# Merge all of the Dataframes from TuitionTracker.com CSV into a singular DataFrame
 df4 = pd.merge(df2, df3, on='Institution Name')
-df5 = pd.merge(df1, df3, left_on='school.name', right_on='Institution Name').drop('Institution Name', axis=1)
 
-
-# retention_rate_df = pd.read_csv('https://www.tuitiontracker.org/data/download/retention-rates.csv', usecols = ['UnitID','Institution Name'])
-#
-# #Combine all CSV files into one large DataSet by Concatenating NOTE: Since all files UnitID's Match Up Pandas auto does this.
-# dataset_df = [colleges_df, sticker_tuition_df, net_tuition_df, grad_rate_df, retention_rate_df]
-# results = pd.concat(dataset_df)
-#
-# print(results)
+# Merge all data from College Scorecard API and TuitionTracker.coms CSV files.
+df5 = pd.merge(df1, df4, on='Institution Name')
 
 
 
